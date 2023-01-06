9da7522a3c99607e317f3f31e84743cda040046e
Ashutosh Pandey <63603603+lazyboson@users.noreply.github.com>
2023-01-06 13:02:17 +0530

    some delay

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -33,12 +33,12 @@ func (r *Router) ListenEvents(req *pb.TaskCreationEventRequest) *pb.TaskEventRes
 		return res
 	}
 
-	r.handleTask(accountId)
+	go r.handleTask(accountId)
 	return res
 }
 
 func (r *Router) handleTask(accountId string) {
-	time.Sleep(2 * time.Second)
+	time.Sleep(10 * time.Second)
 	taskViewUri := r.conf.QueueBaseUrl + taskViewPath
 	fmt.Printf("TaskViewUrl: %+v \n", taskViewUri)
 
